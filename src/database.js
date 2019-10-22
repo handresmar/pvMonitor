0e71574d9e33a22d83b1d88189d71f69b4010e19
handresmar <handresmar@gmail.com>
2019-10-21 21:22:00 -0500

    superado error de mongoose

--- src/database.js
+++ src/database.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/pv-db-app',{
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true 
-})
-.then(db => console.log('DB is connected'))
-.catch(err => console.error(err));
+mongoose.connect('mongodb://localhost/pv-db-app',{useMongoClient:true});
+mongoose.Promise = global.Promise;
+//.then(db => console.log('DB is connected'))
+//.catch(err => console.error(err));
