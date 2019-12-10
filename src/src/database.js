4556a9c8bde576296f7e51a140d4ea57b29acaf7
handresmar <handresmar@gmail.com>
2019-12-10 11:34:05 -0500

    eliminada carpeta src mal agragada

--- src/src/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/pv-db-app',{useMongoClient:true});
-mongoose.Promise = global.Promise;
-//.then(db => console.log('DB is connected'))
-//.catch(err => console.error(err));
